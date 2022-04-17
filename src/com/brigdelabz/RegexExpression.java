63fa604ca19961e83741e165d243fa8b807a987d
prajavalpatki <prajavalpatki.com>
2022-04-18 02:07:26 +0530

    1 special character

--- src/com/brigdelabz/RegexExpression.java
+++ src/com/brigdelabz/RegexExpression.java
@@ -36,7 +36,7 @@ public class RegexExpression {
     }
 
     public boolean password(String password) {
-        String pattern  = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}";
+        String pattern  = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])([#?!@$%^&*-]).{8,}$";
         if (password.matches(pattern)) {
             return true;
         }
